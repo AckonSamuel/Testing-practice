b3826674b1f872c44cfdc8b3bff160270b2a8fc0
AckonSamuel <ackons433@gmail.com>
2022-03-02 10:34:17 +0000

    Add reverse String function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const checkLength = str => str.length >= 1 && str.length <= 10;
 const characterCount = str => checkLength(str) ? str.length : "String lenght must be from 1 to 10";
 
-module.exports = characterCount;
+const reverseStr = str => str.split('').reverse().join('');
+
+module.exports = {characterCount, reverseStr};
