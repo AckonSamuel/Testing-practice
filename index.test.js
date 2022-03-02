18499738e18f93d18afe5a2819e90838e7fe2bc9
AckonSamuel <ackons433@gmail.com>
2022-03-02 10:34:41 +0000

    Add test for reverse String function

--- index.test.js
+++ index.test.js
@@ -1,15 +1,20 @@
-const characterCount = require('./index.js');
+const modules = require('./index.js');
+
+
 
 test('string lenght is less than or equal to 10', () => {
-    expect(characterCount('Microverse')).toBeLessThanOrEqual(10);
+    expect(modules.characterCount('Microverse')).toBeLessThanOrEqual(10);
 })
 
 test('string lenght greater than or equal to 1', () => {
-    expect(characterCount('Microverse')).toBeGreaterThanOrEqual(1);
+    expect(modules.characterCount('Microverse')).toBeGreaterThanOrEqual(1);
 })
 
 
 test('Character count is', () => {
-    expect(characterCount('Microverse')).toBe(10);
+    expect(modules.characterCount('Microverse')).toBe(10);
 });
 
+test('string is reversed', () => {
+    expect(modules.reverseStr("Amen")).toBe("nemA");
+})
