b91c9adf68b748f65c9006297585b458954943c2
AckonSamuel <ackons433@gmail.com>
2022-03-02 10:57:27 +0000

    Add calculator class and methods

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ const checkLength = str => str.length >= 1 && str.length <= 10;
 const characterCount = str => checkLength(str) ? str.length : "String lenght must be from 1 to 10";
 
 const reverseStr = str => str.split('').reverse().join('');
+    class calculator {
+        add= (a, b) => a + b;
+        subtract= (a, b) => a - b;
+        multiply= (a, b) => a * b;
+        divide= (a, b) => a / b;
+    }
 
 module.exports = {characterCount, reverseStr};
